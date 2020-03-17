141e96ab2c14e813098c399b45183d497543cefa
Anoop Thykkandiyil <anoop.thykkandiyil@gmail.com>
2020-03-17 23:15:21 +0530

    Chapter 16 RasteriseState back culling etc implemented

--- Graphics/Graphics.cpp
+++ Graphics/Graphics.cpp
@@ -20,6 +20,7 @@ void Graphics::RenderFrame()
 
 	this->deviceContext->IASetInputLayout(this->vertexshader.GetInputLayout());
 	this->deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	this->deviceContext->RSSetState(this->rasterizerState.Get());
 
 	this->deviceContext->VSSetShader(vertexshader.GetShader(), NULL, 0);
 	this->deviceContext->PSSetShader(pixelshader.GetShader(), NULL, 0);
@@ -112,6 +113,18 @@ bool Graphics::InitializeDirectX(HWND hwnd, int width, int height) {
 
 	// Set the viewport
 	this->deviceContext->RSSetViewports(1, &viewport);
+
+	// Create Rasterizer State
+	D3D11_RASTERIZER_DESC rasterizerDesc;
+	ZeroMemory(&rasterizerDesc, sizeof(rasterizerDesc));
+
+	rasterizerDesc.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
+	rasterizerDesc.CullMode = D3D11_CULL_MODE::D3D11_CULL_BACK;
+	hr = this->device->CreateRasterizerState(&rasterizerDesc, this->rasterizerState.GetAddressOf());
+	if (FAILED(hr)) {
+		ErrorLogger::Log(hr, "Failed to create Rasterizer state.");
+		return false;
+	}
 
 	return true;
 }
